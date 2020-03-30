307e18e93834a58e3d328ce2ea610ca5482a6a4f
p2m3ng <moysan.paul@gmail.com>
2020-03-30 22:27:16 +0200

    SQLCO-9 # [Docs, Refactor] Changed README and simplified the interface

--- sql_converter/query.py
+++ sql_converter/query.py
@@ -1,4 +1,9 @@
 from sql_converter.query_builder.query import QueryBuilder
+from sql_converter.query_builder.database import Table as DatabaseTable
+
+
+class Table(DatabaseTable):
+    pass
 
 
 class Query(QueryBuilder):
